f44a6ecb37021edf1ce6a5c867d7c2714c99511e
Weilence <weilence@163.com>
2024-06-13 16:18:48 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.weilence.com": "https://registry-1.docker.io",
+  "quay.weilence.com": "https://quay.io",
+  "gcr.weilence.com": "https://gcr.io",
+  "k8s-gcr.weilence.com": "https://k8s.gcr.io",
+  "k8s.weilence.com": "https://registry.k8s.io",
+  "ghcr.weilence.com": "https://ghcr.io",
+  "cloudsmith.weilence.com": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
